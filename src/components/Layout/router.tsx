4afd0a69b635cbab22261dc339a945472cf127d5
Zoe Serano <edwardserano@gmail.com>
2024-10-01 09:55:54 +0100

    vite deploy

--- src/components/Layout/router.tsx
+++ src/components/Layout/router.tsx
@@ -25,7 +25,6 @@ const router = createBrowserRouter(
       ],
     },
   ],
-  { basename: import.meta.env.BASE_URL }
 );
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy_react_app_github_pages_vercel",
   css: {
     preprocessorOptions: {
       scss: {
